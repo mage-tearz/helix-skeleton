0355201511e1181012ab7de8f45d1d5ffeac8f89
mage.tearz <137641279+mage-tearz@users.noreply.github.com>
2024-12-06 11:22:07 -0500

    New function naming
    CLASS:OnCanBe isn't referenced anywhere else in the code. CLASS:CanSwitchTo is the actual function

--- schema/classes/sh_police_chief.lua
+++ schema/classes/sh_police_chief.lua
@@ -4,8 +4,8 @@ CLASS.faction = FACTION_POLICE
 
 -- This function will be called whenever the client wishes to become part of this class. If you'd rather have it so this class
 -- has to be set manually by an administrator, you can simply return false to disallow regular users switching to this class.
--- Note that CLASS.isDefault does not add a whitelist like FACTION.isDefault does, which is why we need to use CLASS:OnCanBe.
-function CLASS:OnCanBe(client)
+-- Note that CLASS.isDefault does not add a whitelist like FACTION.isDefault does, which is why we need to use CLASS:CanSwitchTo.
+function CLASS:CanSwitchTo(client)
 	return false
 end
 
